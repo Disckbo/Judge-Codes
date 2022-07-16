83e4e02c8dc3ea16ec20f62dcff560a77c0c6570
Discko500 <kacperek.kacprzak@gmail.com>
2022-07-16 18:30:53 +0200

    1.0r

--- 10340.cpp
+++ 10340.cpp
@@ -1,41 +1,19 @@
 #include <iostream>
 using namespace std;
 
-const int MAX = 3001;
-
-int main() {
-
-    char s[MAX];
-    char t[MAX];
-
-    bool c;
-
-    int j;
-
-    while(cin>>t>>s) {
-
-        int g = sizeof s;
-        int h = sizeof t;
-
-        for (int i = 0; i < g; i++) {
-            if(t[h] == s[i]) {
-                c = true;
-                h--;
-            }
-
-            else {
-                c = false;
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s, t;
+    while (cin >> s >> t) {
+        int t_len = t.length(), s_len = s.length();
+        int j = 0;
+        for (int i = 0; i < t_len; i++) {
+            if (s[j] == t[i]) {
+                j++;
             }
         }
-
-        if(c == true) {
-            cout<<"Yes";
-        }
-
-        else {
-            cout<<"No";
-        }
-
-        return 0;
+        cout << (j == s_len ? "Yes" : "No") << endl;
     }
 }
