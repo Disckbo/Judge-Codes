0ec3608a2659e910cea5f2ba0f08c9c4314fb924
Discko500 <kacperek.kacprzak@gmail.com>
2022-07-21 16:15:12 +0200

    1.0.2

---
+++ 12577.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x=1;
+    char a[15];
+    while(cin>>a) {
+        if(a[0]=='*') {
+            break;
+        }
+        if(strcmp(a,"Hajj")==0) {
+            cout<<"Case "<<x<<": "<<"Hajj-e-Akbar"<<endl;x++;
+        }
+        else if(strcmp(a,"Umrah")==0) {
+            cout<<"Case "<<x<<": "<<"Hajj-e-Asghar"<<endl;x++;
+        }
+    }
+    return 0;
+}
